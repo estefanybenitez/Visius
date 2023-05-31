29fd687912e3f28c3ecc0a2d803440d90d978640
MINEDUCYT <estefany.benitez22@itca.ed.sv>
2023-05-30 21:45:32 -0600

    migracionesE1

--- database/migrations/2023_05_29_202850_create_roles_table.php
+++ database/migrations/2023_05_29_202850_create_roles_table.php
@@ -14,12 +14,6 @@ return new class extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->id('id_rol');
             $table->string('nombre_rol',20);
-            $table->bigInteger('id_users')->unsigned();
-
-            $table->foreign('id_rol')->references('id')->on('users')
-            ->onDelete('cascade')
-            ->onUpdate('cascade');
-
             $table->timestamps();
         });
     }
